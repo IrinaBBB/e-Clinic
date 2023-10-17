0effd8d877a6fc02754069c99cd358bd3e0ac485
IrinaBBB <balkarovairina@gmail.com>
2023-10-17 11:53:33 +0200

    Add ApplicationUser and IdentityContext

---
+++ e-Clinic.DataAccess/Db/IdentityContext.cs
@@ -0,0 +1,12 @@
+﻿using e_Clinic.DataAccess.Entities.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace e_Clinic.DataAccess.Db
+{
+    public class IdentityContext : IdentityDbContext<ApplicationUser>
+    {     
+        public DbSet<ApplicationUser> IdentityUsers { get; set; } = null!;
+    }
+}
+    
---
+++ e-Clinic.DataAccess/Entities/Identity/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace e_Clinic.DataAccess.Entities.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [PersonalData]
+        [MaxLength(255)]
+        public string? FirstName { get; set; }
+
+        [PersonalData]
+        [MaxLength(255)]
+        public string? LastName { get; set; } 
+    }
+}
