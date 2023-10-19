fe8e0e99c05d684d6e2004516a9f9b7fff06a328
IrinaBBB <balkarovairina@gmail.com>
2023-10-19 12:35:49 +0200

    Add Flowbite Styles for Identity

--- e-Clinic.Web/tailwind.config.js
+++ e-Clinic.Web/tailwind.config.js
@@ -2,11 +2,16 @@ module.exports = {
     content: [
         './Pages/**/*.cshtml',
         './Views/**/*.cshtml',
-        './Areas/Identity/Pages/Views/**/*.cshtml'
+        './Areas/Identity/Pages/Account/**/*.cshtml',
+        './Areas/Identity/Pages/Views/Shared/**/*.cshtml',
+        './node_modules/flowbite/**/*.js',
+        
     ],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
 
